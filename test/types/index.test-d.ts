7f8003b7a32cd16ff619338f433dd1e16422feda
kshitijanurag <kshitijanurag2006@gmail.com>
2026-04-07 21:03:56 +0530

    refactor: migrate from `standard` to `neostandard`

--- test/types/index.test-d.ts
+++ test/types/index.test-d.ts
@@ -1,38 +1,38 @@
-import build, { OnUnknown } from "../../index";
-import { expectType } from "tsd";
-import { Transform } from "stream";
+import build, { OnUnknown } from '../../index'
+import { expectType } from 'tsd'
+import { Transform } from 'stream'
 
 /**
  * If enablePipelining is set to true, the function passed as an argument
  * must return a transform. The unknown event should be listened to on the
  * stream passed in the first argument.
  */
-expectType<Transform>(build((source) => source, { enablePipelining: true }));
+expectType<Transform>(build((source) => source, { enablePipelining: true }))
 
 /**
  * If expectPinoConfig is set with enablePipelining, build returns a promise
  */
-expectType<(Promise<Transform>)>(build((source) => source, { enablePipelining: true, expectPinoConfig: true }));
+expectType<(Promise<Transform>)>(build((source) => source, { enablePipelining: true, expectPinoConfig: true }))
 
 /**
  * If enablePipelining is not set the unknown event can be listened to on
  * the returned stream.
  */
-expectType<Transform & OnUnknown>(build((source) => {}));
+expectType<Transform & OnUnknown>(build((source) => {}))
 
 /**
  * If expectPinoConfig is set, build returns a promise
  */
-expectType<(Promise<Transform & OnUnknown>)>(build((source) => {}, { expectPinoConfig: true }));
+expectType<(Promise<Transform & OnUnknown>)>(build((source) => {}, { expectPinoConfig: true }))
 
 /**
  * build also accepts an async function
  */
-expectType<Transform  & OnUnknown>(build(async (source) => {}));
+expectType<Transform & OnUnknown>(build(async (source) => {}))
 
 /**
  * timeKey and levelKey options can be set to customize metadata keys
  */
-expectType<Transform & OnUnknown>(build((source) => {}, { timeKey: 'timestamp' }));
-expectType<Transform & OnUnknown>(build((source) => {}, { levelKey: 'severity' }));
-expectType<Transform & OnUnknown>(build((source) => {}, { timeKey: 'timestamp', levelKey: 'severity' }));
+expectType<Transform & OnUnknown>(build((source) => {}, { timeKey: 'timestamp' }))
+expectType<Transform & OnUnknown>(build((source) => {}, { levelKey: 'severity' }))
+expectType<Transform & OnUnknown>(build((source) => {}, { timeKey: 'timestamp', levelKey: 'severity' }))
